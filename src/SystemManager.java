6fbc943cfb72371814ca9ca46fda53c4e0616a30
Nyikovics Máté <nyikovicsmate@gmail.com>
2019-12-03 18:30:12 +0100

    Increased machineData buffer size.

--- src/SystemManager.java
+++ src/SystemManager.java
@@ -4,7 +4,7 @@ public class SystemManager {
 
     private List<Machine> machines;
     private DatabaseConnector databaseConnector;
-    private List<MachineData> previous5MachineData;
+    private List<MachineData> previous10MachineData;
 
     public void queryMachines() {
 
