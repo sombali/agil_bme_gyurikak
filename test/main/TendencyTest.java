01735aaee7c5e6341ff63c31739e6c5e82b7e772
sombali <sombali97@gmail.com>
2019-12-04 03:10:13 +0100

    TDD2 2

--- test/main/TendencyTest.java
+++ test/main/TendencyTest.java
@@ -1,23 +1,45 @@
 package main;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import javax.crypto.Mac;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class TendencyTest {
+    private SystemManager systemManager;
 
-    @Test
-    public void testEmptyPrevious10() {
+    @Before
+    public void setUp() {
         Machine machine = new Machine(10,10);
         List<Machine> machines = new ArrayList<>();
         machines.add(machine);
 
-        SystemManager systemManager = new SystemManager(machines);
+        systemManager = new SystemManager(machines);
+    }
+
+    @Test
+    public void testEmptyPrevious10() {
         StatusCode result = systemManager.checkPreviousTenTendency();
 
-        assertEquals(result, StatusCode.OK);
+        assertEquals(StatusCode.OK, result);
+    }
+
+    @Test
+    public void testNotIncreasingTendency() {
+        addDatas(systemManager.getMachines().get(0), new double[] {1,2,3,3,3,4,3,3,4,3});
+
+        StatusCode result = systemManager.checkPreviousTenTendency();
+
+        assertEquals(StatusCode.OK, result);
+    }
+
+    private void addDatas(Machine machine, double[] powerDatas) {
+        for(double data: powerDatas) {
+            machine.addMachineData(new MachineData(0, data));
+        }
     }
 }
