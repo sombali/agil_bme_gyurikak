56f432a578c8c074d029c2e97ddd774283fe1875
Nyikovics Máté <nyikovicsmate@gmail.com>
2019-12-09 22:26:06 +0100

    tdd 1 red

--- test/main/MateTests.java
+++ test/main/MateTests.java
@@ -1,7 +1,21 @@
 package main;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+
 public class MateTests {
 
+    @Test
+    public void checkSystemThreshold() {
+        Machine m = new Machine(1);
+        List<Machine> machines = new ArrayList<>();
+        machines.add(m);
+        SystemManager sm = new SystemManager(machines);
+        assertEquals(sm.checkPowerLimit(), StatusCode.OK);
+    }
 }
 
 
